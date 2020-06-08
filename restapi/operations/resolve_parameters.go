163a98045464c9e10aba5a9f7dae046200cb8c49
lzxm160 <lzxm160@sina.com>
2020-06-08 14:07:31 +0800

    x

--- restapi/operations/resolve_parameters.go
+++ restapi/operations/resolve_parameters.go
@@ -6,6 +6,7 @@ package operations
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-openapi/errors"
@@ -42,7 +43,7 @@ type ResolveParams struct {
 // To ensure default values, the struct must have been initialized with NewResolveParams() beforehand.
 func (o *ResolveParams) BindRequest(r *http.Request, route *middleware.MatchedRoute) error {
 	var res []error
-
+	fmt.Println(r.Method,r.RequestURI)
 	o.HTTPRequest = r
 
 	rIdentifier, rhkIdentifier, _ := route.Params.GetOK("identifier")
